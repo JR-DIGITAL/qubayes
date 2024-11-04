3816ffd829e863101cebdd81d91666ef1e96f956
Florian Krebs <florian.krebs@joanneum.at>
2024-11-04 15:29:23 +0100

    first version ready

--- qubayes/variational_inference.py
+++ qubayes/variational_inference.py
@@ -14,7 +14,6 @@ from tensorflow.keras import layers
 from copy import deepcopy
 
 
-
 class Optimizer(object):
 
     def __init__(self, born_machine, bayes_net, classifier=None, n_iterations=100, learning_rate=0.003):
@@ -25,7 +24,7 @@ class Optimizer(object):
         self.classifier = classifier
         self.initialize_classifier()
 
-    def estimate_gradient(self):
+    def estimate_gradient(self, n_samples=100):
         # Use parameter shift rule for estimation, as outlined in B15 in the paper
         shift = np.pi / 2
         gradients = np.zeros(self.born_machine.params.shape)
@@ -41,10 +40,10 @@ class Optimizer(object):
 
             for key in ['plus', 'minus']:
                 # Sample 50 points
-                samples_crs = bm[key].sample(50)
+                samples_crs = bm[key].sample(n_samples)
                 # classify 50 points
                 # TODO: Really logit?
-                logit_d = self.classifier.predict(samples_crs)[:, 0]
+                logit_d = self.classifier.predict(samples_crs, verbose=0)[:, 0]
                 # compute P(x|z) for the 50 points
                 loglik = self.bayes_net.compute_log_likelihood(samples_crs)
                 # compute the mean difference (logit(d_i) - log(p(x_i|z_i))) ->
@@ -67,14 +66,18 @@ class Optimizer(object):
         self.classifier.compile(optimizer=optimizer,
                                 loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),  # we already have a sigmoid after the last layer
                                 metrics=['accuracy'])
-        self.classifier.fit(x=train_x, y=train_y, epochs=20, batch_size=10)
+        self.classifier.fit(x=train_x, y=train_y, epochs=50, batch_size=10,
+                            verbose=0)
         return
 
-    def compute_loss(self):
-        return 1
+    def compute_loss(self, samples_crs):
+        logit_d = self.classifier.predict(samples_crs, verbose=0)[:, 0]
+        loglik = self.bayes_net.compute_log_likelihood(samples_crs)
+        return (logit_d - loglik).mean()
 
     def optimize(self):
         s_prior = self.bayes_net.sample_from_joint(100)
+        tvd = np.zeros((self.n_iterations,))
         for i in range(self.n_iterations):
             # Draw 100 sample from the born machine
             s_bm = self.born_machine.sample(100)
@@ -86,17 +89,24 @@ class Optimizer(object):
             self.train_classifier(x_train, y_train)
 
             # Calculate the gradient using the parameter-shift rule
-            gradient = self.estimate_gradient()
+            gradients = self.estimate_gradient()
 
             # Update the parameter (for maximization, we add the gradient)
-            self.bm.params += self.learning_rate * gradient
+            self.born_machine.params += self.learning_rate * gradients
 
             # Evaluate the function at the new theta
-            loss = self.compute_loss()
+            loss = self.compute_loss(s_bm)
+            tvd[i] = self.compute_tvd(s_bm)
 
             # Print the current theta and function value
-            print(f"Iteration {i + 1}: loss = {loss:.4f}, mean gradient = {gradient.mean():.4f}")
-        return self.bm
+            print(f"Iteration {i + 1}: tvd = {tvd[i]:.4f}, loss = {loss:.4f}, mean gradient = {gradients.mean():.4f}")
+        return self.born_machine, tvd
+
+    def compute_tvd(self, samples):
+        # Compute total variation distance (The largest absolute difference
+        # between the probabilities that the two probability distributions
+        # assign to the same event.).
+        return self.bayes_net.compute_tvd(samples)
 
 
 class BornMachine(object):
@@ -132,10 +142,8 @@ class BornMachine(object):
 
         # Simulate the circuit
         simulator = AerSimulator(method='matrix_product_state')
-        # simulator = Aer.get_backend('aer_simulator')
         compiled_circuit = transpile(circuit, simulator)
         result = simulator.run(compiled_circuit, shots=n_samples, memory=True).result()
-        # counts = dict(result.get_counts())
         samples = result.get_memory()
         samples = np.array([[char == '1' for char in string] for string in samples], dtype='int32')
         return samples
@@ -143,8 +151,28 @@ class BornMachine(object):
 
 class SprinklerBN(object):
 
-    def __init__(self):
+    def __init__(self, random_cpd=True):
+        self.random_cpd = random_cpd
         self.graph = create_sprinkler_graph()
+        if random_cpd:
+            self.graph.nodes['cloudy'].data[0] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['cloudy'].data[1] = 1. - self.graph.nodes['cloudy'].data[0]
+            self.graph.nodes['rain'].data[0, 0] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['rain'].data[1, 0] = 1. - self.graph.nodes['rain'].data[0, 0]
+            self.graph.nodes['rain'].data[0, 1] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['rain'].data[1, 1] = 1. - self.graph.nodes['rain'].data[0, 1]
+            self.graph.nodes['sprinkler'].data[0, 0] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['sprinkler'].data[1, 0] = 1. - self.graph.nodes['sprinkler'].data[0, 0]
+            self.graph.nodes['sprinkler'].data[0, 1] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['sprinkler'].data[1, 1] = 1. - self.graph.nodes['sprinkler'].data[0, 1]
+            self.graph.nodes['wet'].data[0, 0, 0] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['wet'].data[1, 0, 0] = 1. - self.graph.nodes['wet'].data[0, 0, 0]
+            self.graph.nodes['wet'].data[0, 0, 1] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['wet'].data[1, 0, 1] = 1. - self.graph.nodes['wet'].data[0, 0, 1]
+            self.graph.nodes['wet'].data[0, 1, 0] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['wet'].data[1, 1, 0] = 1. - self.graph.nodes['wet'].data[0, 1, 0]
+            self.graph.nodes['wet'].data[0, 1, 1] = np.random.uniform(low=0.01, high=0.99)
+            self.graph.nodes['wet'].data[1, 1, 1] = 1. - self.graph.nodes['wet'].data[0, 1, 1]
 
     def sample_from_joint(self, n_samples):
         s_prior = self.graph.sample_from_graph(n_samples)
@@ -163,8 +191,33 @@ class SprinklerBN(object):
     def compute_log_likelihood(self, n_samples):
         return self.compute_log_p_w_crs(n_samples)
 
+    def compute_tvd(self, samples_crs):
+        # Total variation distance between Q and P(C, R, S | W = 1)
+        # 1) Get the counts of the samples -> Q
+        unique_rows, unique_counts = np.unique(samples_crs, axis=0, return_counts=True)
+        estimation = np.zeros((2, 2, 2), dtype=float)  # C, R, S
+        for c in range(2):
+            for r in range(2):
+                for s in range(2):
+                    idx = (unique_rows == np.array([c, r, s])).all(axis=1)
+                    if idx.any():
+                        estimation[c, r, s] = float(unique_counts[idx][0]) / samples_crs.shape[0]
 
+        # 2) Get the exact probabilities -> P
+        posterior = np.zeros((2, 2, 2))  # C, R, S
+        for c in range(2):
+            prob = self.graph.nodes['cloudy'].data[c]
+            for r in range(2):
+                prob2 = prob * self.graph.nodes['rain'].data[r, c]
+                for s in range(2):
+                    prob3 = prob2 * self.graph.nodes['sprinkler'].data[s, c]
+                    prob3 *= self.graph.nodes['wet'].data[1, s, r]
+                    posterior[c, r, s] = prob3
+        posterior /= posterior.sum()
 
+        # 3) Find max distance
+        tvd = (abs(posterior - estimation)).max()
+        return tvd
 
 
 if __name__ == "__main__":
@@ -174,5 +227,5 @@ if __name__ == "__main__":
     bm = BornMachine(3, n_blocks=1)
     bm.print_circuit()
     # Optimize it
-    opt = Optimizer(bm, sprinkler_bn, n_iterations=3, learning_rate=0.003)
-    opt.optimize()
+    opt = Optimizer(bm, sprinkler_bn, n_iterations=200, learning_rate=0.003)
+    bm_opt = opt.optimize()
